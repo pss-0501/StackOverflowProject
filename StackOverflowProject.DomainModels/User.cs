a24cdf0343696fcd74cc824642ba5e60c9b8be88
psawant5070@gmail.com <psawant5070@gmail.com>
2023-03-21 00:36:03 +0530

    user model

---
+++ StackOverflowProject.DomainModels/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflowProject.DomainModels
+{
+    public class User
+    { 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+        public int Email { get; set; }
+        public int PasswordHash { get; set; }
+        public int Name { get; set; }
+        public int Mobile { get; set; }
+        public int IsAdmin { get; set; }
+
+    }
+}
