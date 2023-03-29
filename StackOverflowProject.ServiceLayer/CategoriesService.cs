0e971c2769ac8551da6291d17d58ebd0d9e89f76
psawant5070@gmail.com <psawant5070@gmail.com>
2023-03-30 01:02:05 +0530

    service

---
+++ StackOverflowProject.ServiceLayer/CategoriesService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflowProject.DomainModels;
+using StackOverflowProject.Repositories;
+using StackOverflowProject.ViewModels;
+using AutoMapper;
+using AutoMapper.Configuration;
+
+namespace StackOverflowProject.ServiceLayer
+{
+    public interface ICategoriesService
+    {
+
+    }
+
+    public class CategoriesService
+    {
+    }
+}
